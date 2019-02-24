5d23126c5d37aec2f7f0be0796632b42d54ac3ac
gusttavo212 <gusttavo212@gmail.com>
2019-02-24 17:06:06 -0300

    Migration

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: 'localhost',
+  username: 'gadsden',
+  password: '250433',
+  dialect: 'postgres',
+  operatosAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+
+};
