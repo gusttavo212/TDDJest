d0d75be62b803a7f4897cc8ed3acc4ef718b9be8
gusttavo212 <gusttavo212@gmail.com>
2019-02-24 16:01:59 -0300

    Base Config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+      "import"
+    ],
+    "rules": {
+      "no-console": "off",
+      "no unused-vars": "off",
+      "max-len": 0,
+      "prefer-destructuring": "off",
+      "import/newline-after-import": "off",
+      "no-undef": "off",
+      "object-shorthand": "off"
+    }
+  
+  };
+  
